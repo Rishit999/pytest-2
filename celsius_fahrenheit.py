14cf4208074652df5b62de0e7c859fff92f4d963
Rishit999 <rishitnayakar@gmail.com>
2026-01-18 19:20:00 +0530

    Update and rename employee.py to celsius_fahrenheit.py

---
+++ celsius_fahrenheit.py
@@ -0,0 +1,5 @@
+def celsius_to_fahrenheit(celsius=0):
+ fahrenheit = (celsius * 9/5) + 32
+ return f"Temperature in Fahrenheit: {fahrenheit}"
+if __name__ == "__main__":
+ print(celsius_to_fahrenheit())
